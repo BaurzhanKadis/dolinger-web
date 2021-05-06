6429bc9c7590b09a233cb9d92dbf1e083e4614b4
baurzhan <todd-45@mail.ru>
2021-05-07 00:36:00 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,349 @@
+window.onload = () => {
+  console.log("start page");
+
+  const btnReg = document.querySelector(".top_item-btn_grnd");
+  const b = document.querySelector("body");
+  const fixC = document.querySelector(".fix_callback");
+  const closeModal = document.querySelector(".close_window");
+  const pole = document.querySelector(".pole");
+  const submit = document.querySelector(".submit_baur");
+  const btnGood = document.querySelector(".btn_good-close");
+  const items = document.querySelectorAll(".callback-good .callback-item");
+
+  if (window.innerWidth <= 768) {
+    document.querySelector(
+      ".rating-item-left .title-rating"
+    ).innerHTML = document
+      .querySelector(".rating-item-left .title-rating")
+      .innerHTML.replace("<br>", " ");
+    document
+      .querySelectorAll(".name-contact")
+      .forEach((i) => (i.innerHTML = i.innerHTML.replace("<br>", " ")));
+    document
+      .querySelector("#contacts")
+      .before(document.querySelector("#clients"));
+    document
+      .querySelectorAll(".shift")
+      .forEach((e) => (e.innerHTML = "Оставить заявку"));
+  }
+
+  btnReg.addEventListener("click", () => {
+    fixC.classList.add("fix_callback-active");
+    b.classList.add("body-hidden");
+  });
+
+  submit.addEventListener("click", () => {
+    items.forEach((item) => {
+      item.classList.add("dn");
+    });
+    document.querySelector(".good").classList.add("db");
+    document.querySelector(".modal-container").classList.add("wa");
+  });
+
+  const btnClose = (e) => {
+    e.addEventListener("click", () => {
+      fixC.classList.remove("fix_callback-active");
+      b.classList.remove("body-hidden");
+      if (document.querySelector(".modal-container").classList.contains("wa")) {
+        console.log("test");
+        return items.forEach((item) => {
+          item.classList.remove("dn");
+          document.querySelector(".good").classList.remove("db");
+          document.querySelector(".modal-container").classList.remove("wa");
+        });
+      }
+    });
+  };
+
+  btnClose(closeModal);
+  btnClose(pole);
+  btnClose(btnGood);
+
+  let tabsPP = document.getElementsByClassName("tab_pp");
+  let tabs = document.querySelectorAll(".tab_dop");
+
+  let sectionsPP = document.getElementsByClassName("ppm_bottum-item");
+  let sections = document.getElementsByClassName("dfm_bottum-item");
+
+  for (let i = 0; i < tabsPP.length; i++) {
+    tabsPP[i].onclick = tabclickPP;
+  }
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].onclick = tabclick;
+  }
+
+  function tabclickPP(event) {
+    let tab = event.target;
+    if (event.target.localName === "span") {
+      tab = event.target.parentElement;
+    }
+    let tabId = tab.dataset.id;
+
+    for (let k = 0; k < tabs.length; k++) {
+      tabsPP[k].classList.remove("active");
+      tabsPP[tabId - 1].classList.add("active");
+
+      sectionsPP[k].classList.remove("active");
+      sectionsPP[tabId - 1].classList.add("active");
+    }
+  }
+  function tabclick(event) {
+    let tab = event.target;
+    if (event.target.localName === "span") {
+      tab = event.target.parentElement;
+    }
+    let tabId = tab.dataset.id;
+
+    for (let k = 0; k < tabs.length; k++) {
+      tabs[k].classList.remove("active");
+      tabs[tabId - 1].classList.add("active");
+
+      sections[k].classList.remove("active");
+      sections[tabId - 1].classList.add("active");
+    }
+  }
+
+  if (window.innerWidth <= 992) {
+    document.querySelector('.admin').after(document.querySelector('.admin-three'))
+  } else {
+    document.querySelector('.admin').append(document.querySelector('.admin-three'))
+  }
+
+  const sliderClick = (offset, padding = 20) => {
+    document.querySelector('.admin').addEventListener('mousedown', (e) => {
+      console.log(e.offsetX,'e.offsetX')
+      if (e.offsetX < point) {
+        document.querySelector('.admin').style.transform = `translateX(${(offset-padding)-(widthItemSlider+30)}px)`;//margin beetwen of items-sliders
+        document.querySelector('.prev').classList.remove('active-dot')
+        document.querySelector('.next').classList.add('active-dot')
+      } else {
+        document.querySelector('.admin').style.transform = `translateX(${(offset-padding)}px)`;
+        document.querySelector('.prev').classList.add('active-dot')
+        document.querySelector('.next').classList.remove('active-dot')
+      }
+    })
+  }
+  
+  const centerItemSlider = () => {
+    let offsetParent = window.innerWidth/2;
+    let offsetChild = widthItemSlider/2;
+    let offset = offsetParent - offsetChild;
+    point = offsetChild;
+    document.querySelector('.admin').style.transform = `translateX(${offset-20}px)`;
+    console.log(offset, 'offset');
+    sliderClick(offset);
+  }
+
+  let point;
+  let widthItemSlider = document.querySelector('.admin-one').offsetWidth;
+  if (window.innerWidth <= 480) {
+    centerItemSlider();
+  } else {
+    document.querySelector('.admin').style.transform = `translateX(0)`;
+  }
+};
+
+window.onresize = () => {
+  if (window.innerWidth <= 992) {
+    document.querySelector('.admin').after(document.querySelector('.admin-three'))
+  } else {
+    document.querySelector('.admin').append(document.querySelector('.admin-three'))
+  }
+}
+
+const baurWave = (id, colorStart, colorEnd, heightWave, AM) => {
+  let c = document.getElementById(id),
+    ctx = c.getContext("2d"),
+    cw = (c.width = window.innerWidth),
+    grdline = ctx.createLinearGradient(0, 0, cw, 0);
+  grdline.addColorStop(0, colorStart);
+  grdline.addColorStop(1, colorEnd);
+  // ch = c.height = window.innerHeight,
+  (ch = c.height = window.innerWidth <= 375 ? 500 : heightWave),
+    (points = []),
+    (tick = 0),
+    (opt = {
+      count: 5,
+      range: {
+        x: 10,
+        y: AM,
+      },
+      duration: {
+        min: 500,
+        max: 500,
+      },
+      thickness: 1,
+      strokeColor: grdline,
+      level: .5,
+      curved: true,
+    }),
+    (rand = function (min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }),
+    (ease = function (t, b, c, d) {
+      if ((t /= d / 2) < 1) return (c / 2) * t * t + b;
+      return (-c / 2) * (--t * (t - 2) - 1) + b;
+    });
+
+  ctx.lineJoin = "round";
+  ctx.lineWidth = opt.thickness;
+  ctx.strokeStyle = opt.strokeColor;
+
+  let Point = function (config) {
+    this.anchorX = config.x;
+    this.anchorY = config.y;
+    this.x = config.x;
+    this.y = config.y;
+    this.setTarget();
+  };
+
+  Point.prototype.setTarget = function () {
+    this.initialX = this.x;
+    this.initialY = this.y;
+    this.targetX = this.anchorX + rand(0, opt.range.x * 2) - opt.range.x;
+    this.targetY = this.anchorY + rand(0, opt.range.y * 2) - opt.range.y;
+    this.tick = 0;
+    this.duration = rand(opt.duration.min, opt.duration.max);
+  };
+
+  Point.prototype.update = function () {
+    let dx = this.targetX - this.x;
+    let dy = this.targetY - this.y;
+    let dist = Math.sqrt(dx * dx + dy * dy);
+
+    if (Math.abs(dist) <= 0) {
+      this.setTarget();
+    } else {
+      let t = this.tick;
+      let b = this.initialY;
+      let c = this.targetY - this.initialY;
+      let d = this.duration;
+      this.y = ease(t, b, c, d);
+
+      b = this.initialX;
+      c = this.targetX - this.initialX;
+      d = this.duration;
+      this.x = ease(t, b, c, d);
+
+      this.tick++;
+    }
+  };
+
+  Point.prototype.render = function () {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 3, 0, Math.PI * 2, false);
+    ctx.fillStyle = "#000";
+    ctx.fill();
+  };
+
+  let updatePoints = function () {
+    let i = points.length;
+    while (i--) {
+      points[i].update();
+    }
+  };
+
+  let renderPoints = function () {
+    let i = points.length;
+    while (i--) {
+      points[i].render();
+    }
+  };
+
+  let renderShape = function () {
+    ctx.beginPath();
+    let pointCount = points.length;
+    ctx.moveTo(points[0].x, points[0].y);
+    let i;
+    for (i = 0; i < pointCount - 1; i++) {
+      let c = (points[i].x + points[i + 1].x) / 2;
+      let d = (points[i].y + points[i + 1].y) / 2;
+      ctx.quadraticCurveTo(points[i].x, points[i].y, c, d);
+    }
+    ctx.lineTo(-opt.range.x - opt.thickness, ch + opt.thickness);
+    ctx.lineTo(cw + opt.range.x + opt.thickness, ch + opt.thickness);
+    ctx.closePath();
+    let grd = ctx.createLinearGradient(0, 0, cw, 0);
+    grd.addColorStop(0, colorStart);
+    grd.addColorStop(1, colorEnd);
+
+    ctx.fillStyle = grd;
+    // ctx.fillStyle = 'hsl('+(tick/2)+', 80%, 60%)';
+    // ctx.fillRect(20,20,150,100);
+    ctx.fill();
+    ctx.stroke();
+  };
+
+  let clear = function () {
+    ctx.clearRect(0, 0, cw, ch);
+  };
+
+  let loop = function () {
+    window.requestAnimFrame(loop, c);
+    tick++;
+    clear();
+    updatePoints();
+    renderShape();
+    //renderPoints();
+  };
+
+  let i = opt.count + 2;
+  let spacing = (cw + opt.range.x * 2) / (opt.count - 1);
+  while (i--) {
+    points.push(
+      new Point({
+        x: spacing * (i - 1) - opt.range.x,
+        y: ch - ch * opt.level,
+      })
+    );
+  }
+
+  window.requestAnimFrame = (function () {
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function (a) {
+        window.setTimeout(a, 1e3 / 60);
+      }
+    );
+  })();
+
+  loop();
+};
+
+const idWaveCTW = "canvas_top_wave",
+colorStartCTW = "#F3E7E7",
+colorEndCTW = "#FFFCE3";
+
+const idWaveCBW = "canvas_bottum_wave",
+colorStartCBW = '#F3E7E7',
+colorEndCBW = "#FFFCE3";
+
+const idWaveC = "c",
+colorStartC = "#F3E7E9",
+colorEndC = "#E3EEFF";
+
+const idDesignWave = "design_wave",
+colorStartDW = '#EEF8ED',
+colorEndDW = '#EAF4EB';
+
+const idOrderW = "order_wave",
+colorStartOW = '#EEF8ED',
+colorEndOW = '#EAF4EB';
+
+baurWave(idWaveCTW, colorStartCTW, colorEndCTW, 440, 100);
+baurWave(idWaveCBW, colorStartCBW, colorEndCBW, 600, 200);
+baurWave(idWaveC, colorStartC, colorEndC, 360, 80);
+baurWave(idDesignWave, colorStartDW, colorEndDW, 700, 300);
+baurWave(idOrderW, colorStartOW, colorEndOW, 500, 200);
+
+// count — количество точек в волне, на которых будут происходить колебания;
+// range — амплитуды колебаний по оси x и y;
+// duration — скорость колебаний;
+// thickness — толщина верхней линии волны;
+// strokeColor — цвет верхней линии волны;
+// level — насколько волна заполнит собой высоту блока;
+// строка ctx.fillStyle = ‘hsl(‘+(tick/2)+’, 80%, 60%)’; — задает изменение цвета в цветовой модели HSL. Чтобы цвет не менялся, измените строку например на такую — ctx.fillStyle = ‘hsl(0, 80%, 60%)’, тогда волна будет всегда красного цвета.
